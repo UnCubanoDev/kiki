d979a829805c26592284c0f0bdd099d4aa1261ab
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-06-29 12:46:52 -0400

    revert

--- src/settings.py
+++ src/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "static"
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
