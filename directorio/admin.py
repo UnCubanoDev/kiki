a7e481f07b237033df4ef72845c02cd2847d2a9e
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-07-04 16:28:37 -0400

    fix auth in admin

--- directorio/admin.py
+++ directorio/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import User
 
-# Register your models here.
-admin.site.register(User)
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+
+
+admin.site.register(User, UserAdmin)
